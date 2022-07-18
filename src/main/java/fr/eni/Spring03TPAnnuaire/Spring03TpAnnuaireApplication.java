ad4ef1defa0fc5f359bfd3cfbf0a840dc980309b
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-07-18 15:32:40 +0200

    CREATE

---
+++ src/main/java/fr/eni/Spring03TPAnnuaire/Spring03TpAnnuaireApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.Spring03TPAnnuaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring03TpAnnuaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring03TpAnnuaireApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/Spring03TPAnnuaire/Spring03TpAnnuaireApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.Spring03TPAnnuaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring03TpAnnuaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
